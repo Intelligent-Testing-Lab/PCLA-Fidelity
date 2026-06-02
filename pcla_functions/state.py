f624677612f253bea68a68523601ae89ffb960a0
David Gasinski <dgasinski1@sheffield.ac.uk>
2026-06-02 15:13:44 +0100

    (fix) added conversion to bgr from bgra

--- pcla_functions/state.py
+++ pcla_functions/state.py
@@ -59,7 +59,7 @@ class CameraState:
 
         try:
             for frame in buffer:
-                CameraState.save_numpy_frame(frame)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
                 video_writer.write(frame)
         finally:
             video_writer.release()
